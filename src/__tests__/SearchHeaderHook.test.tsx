37924de12ebde4b3555370c3fad5fe9f005be4f8
Josh <sunqixuanno1@126.com>
2022-04-01 14:28:42 -0400

    add logintest render

--- src/__tests__/SearchHeaderHook.test.tsx
+++ src/__tests__/SearchHeaderHook.test.tsx
@@ -1,8 +1,10 @@
 import { renderHook, act } from '@testing-library/react-hooks';
+
 import SearchHeader from "../components/SearchHeader";
+import Login from "../components/Login";
 
 
-test('render search header using hook', () =>{
+test('render login using hook', () =>{
     const { result } = renderHook(() => SearchHeader())
-    console.log(result)
+    console.log(result.current)
 })
--- src/__tests__/SearchHeaderReact.test.tsx
+++ src/__tests__/SearchHeaderReact.test.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 import SearchHeader from "../components/SearchHeader";
+import Login from "../components/Login";
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
+import App from "../App";
 
 
 let container:any = null;
@@ -20,6 +23,12 @@ afterEach(() => {
 
 it("render search header", () => {
     act(() => {
-        render(<SearchHeader/>, container);
+        render(
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/login'} element={<Login/>}/>
+                </Routes>
+            </BrowserRouter>,
+            container);
     });
 })
